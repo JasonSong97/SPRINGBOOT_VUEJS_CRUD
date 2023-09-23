a1fe8d1aad5f5b9837bf3508ed92343fc6c9c571
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-23 16:25:32 +0900

    feat : id로 jwt 만들어 cookie에 담기

--- src/main/java/com/joshua/gallery/backend/controller/api/AccountApiController.java
+++ src/main/java/com/joshua/gallery/backend/controller/api/AccountApiController.java
@@ -2,14 +2,19 @@ package com.joshua.gallery.backend.controller.api;
 
 import com.joshua.gallery.backend.model.member.Member;
 import com.joshua.gallery.backend.model.member.MemberRepository;
+import com.joshua.gallery.backend.service.JwtService;
+import com.joshua.gallery.backend.service.JwtServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @RequestMapping("/api")
@@ -20,10 +25,19 @@ public class AccountApiController {
      MemberRepository memberRepository;
 
      @PostMapping("/account/login")
-     public int login(@RequestBody Map<String, String> params) {
+     public ResponseEntity login(@RequestBody Map<String, String> params, HttpServletResponse res) {
           Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
           if (member != null) {
-               return member.getId();
+               JwtService jwtService = new JwtServiceImpl();
+               int id = member.getId();
+               String token = jwtService.getToken("id", id);
+
+               Cookie cookie = new Cookie("token", token);
+               cookie.setHttpOnly(true); // 자바스크립트 접근 불가
+               cookie.setPath("/");
+
+               res.addCookie(cookie);
+               return new ResponseEntity<>(id, HttpStatus.OK);
           }
 
           throw new ResponseStatusException(HttpStatus.NOT_FOUND);
