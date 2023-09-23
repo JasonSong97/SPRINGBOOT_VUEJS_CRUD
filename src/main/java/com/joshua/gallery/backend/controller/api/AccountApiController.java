dc63735f6f1251f5d4194d668d1dae8be14018c4
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-23 16:00:00 +0900

    fix : 로그인 실패시 에러 처리

--- src/main/java/com/joshua/gallery/backend/controller/api/AccountApiController.java
+++ src/main/java/com/joshua/gallery/backend/controller/api/AccountApiController.java
@@ -3,10 +3,12 @@ package com.joshua.gallery.backend.controller.api;
 import com.joshua.gallery.backend.model.member.Member;
 import com.joshua.gallery.backend.model.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
 
@@ -24,6 +26,6 @@ public class AccountApiController {
                return member.getId();
           }
 
-          return 0;
+          throw new ResponseStatusException(HttpStatus.NOT_FOUND);
      }
 }
