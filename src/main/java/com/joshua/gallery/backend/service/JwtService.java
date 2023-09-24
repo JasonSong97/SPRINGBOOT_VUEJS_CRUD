9d214d08c8352f3748b89916801369cea9ed271d
Jason_song97 <jaegeunsong97@gmail.com>
2023-09-24 12:52:35 +0900

    feat : 요청한 사용자 확인, 토큰에서 id 가져오기

--- src/main/java/com/joshua/gallery/backend/service/JwtService.java
+++ src/main/java/com/joshua/gallery/backend/service/JwtService.java
@@ -7,4 +7,10 @@ public interface JwtService {
     String getToken(String key, Object value);
 
     Claims getClaims(String token);
+
+    // 요청한 사용자가 올바른지 확인
+    boolean isValid(String token);
+
+    // 토큰 정보에서 사용자 Id 가져오기
+    int getId(String token);
 }
--- src/main/java/com/joshua/gallery/backend/service/JwtServiceImpl.java
+++ src/main/java/com/joshua/gallery/backend/service/JwtServiceImpl.java
@@ -53,8 +53,21 @@ public class JwtServiceImpl implements JwtService{
             } catch (JwtException e) {
                 // 유효하지 않음
             }
-
         }
-        return null;
+        return null; // 문제가 있는 경우 null 리턴
+    }
+
+    @Override
+    public boolean isValid(String token) {
+        return this.getClaims(token) != null;
+    }
+
+    @Override
+    public int getId(String token) {
+        Claims claims = this.getClaims(token);
+        if (claims != null) {
+            return Integer.parseInt(claims.get("id").toString());
+        }
+        return 0; // null
     }
 }
